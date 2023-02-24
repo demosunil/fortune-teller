3fc38c7f2f9d44e7f3e49ece94a50077532c7373
SUNILKUMAR PATEL <scpatel1908@gmail.com>
2023-02-24 04:22:53 +0000

    commit1

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -10,4 +10,29 @@ class NumbersController < ApplicationController
 
     render({ :template => "lottery_stuff/woohoo.html.erb"})
   end 
+  def losers
+    @zebra = Array.new
+
+    5.times do
+      giraffe = rand(1...100)
+      
+      @zebra.push(giraffe)
+    end
+
+    render({ :template => "lottery_stuff/unlucky.html.erb"})
+  end 
+  def dice_1
+    @zebra = Array.new
+
+    1.times do
+      giraffe = rand(1..6)
+      
+      @zebra.push(giraffe)
+    end
+
+    render({ :template => "dice/dice1.html.erb"})
+  end 
+
+
+
 end
